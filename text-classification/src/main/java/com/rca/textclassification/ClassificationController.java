7c1e2dd573ea2a163743d1327b74c9981d45e6ff
Yves HAKIZIMANA <yvhakizimana123@gmail.com>
2024-07-01 21:45:07 +0200

    Class Exposing endpoints for classification for the applications.

---
+++ text-classification/src/main/java/com/rca/textclassification/ClassificationController.java
@@ -0,0 +1,34 @@
+package com.rca.textclassification;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ClassificationController {
+
+    private final TextClassifier textClassifier;
+
+    ClassificationController(TextClassifier textClassifier){
+        this.textClassifier = textClassifier;
+    }
+
+    //Classifying the texts in terms of business, technology or sport without any context.
+    @PostMapping("/classify/class-names")
+    String classifyClassNames(@RequestBody String text){
+        return textClassifier.classifyClassNames(text);
+    }
+
+    @PostMapping("/classify/class-descriptions")
+    String classifyClassDescriptions(String text){
+        return textClassifier.classifyClassDescriptions(text);
+    }
+
+    @PostMapping("/classify/few-shots-prompt")
+    String classifyFewShotsPrompt(@RequestBody String text) {
+        return textClassifier.classifyFewShotPrompts(text);
+    }
+
+
+
+}
