1bf3e9b7cc4c367229f828eaea506a7491a19a21
Yves HAKIZIMANA <yvhakizimana123@gmail.com>
2024-07-01 21:13:39 +0200

    Initializing the chat module with ollama

---
+++ 01-chat-models-ollama/src/main/java/com/rca/ollama/ChatController.java
@@ -0,0 +1,22 @@
+package com.rca.ollama;
+
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ChatController {
+
+    private final ChatModel chatModel;
+
+    public ChatController(ChatModel chatModel){
+        this.chatModel = chatModel;
+    }
+
+    @GetMapping("/chat")
+    String chat(@RequestParam(defaultValue = "What did Gandalf say to the Balrog?") String message) {
+        return chatModel.call(message);
+    }
+
+}
