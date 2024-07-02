223372d537f498bb66cf74cca50c2411cced9a3d
Yves HAKIZIMANA <yvhakizimana123@gmail.com>
2024-07-02 13:00:15 +0200

    QuestionAnswering.

--- question-answering/src/main/java/com/rca/questionanswering/QuestionAnsweringApplication.java
+++ question-answering/src/main/java/com/rca/questionanswering/QuestionAnsweringApplication.java
@@ -1,7 +1,11 @@
 package com.rca.questionanswering;
 
+import org.springframework.ai.embedding.EmbeddingModel;
+import org.springframework.ai.vectorstore.SimpleVectorStore;
+import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class QuestionAnsweringApplication {
@@ -9,5 +13,11 @@ public class QuestionAnsweringApplication {
     public static void main(String[] args) {
         SpringApplication.run(QuestionAnsweringApplication.class, args);
     }
+
+    @Bean
+    VectorStore vectorStore(EmbeddingModel embeddingModel){
+        return new SimpleVectorStore(embeddingModel);
+    }
+
 
 }
