5500252f440811fc55b0b8e546eb4fc549ae8626
Yves HAKIZIMANA <yvhakizimana123@gmail.com>
2024-07-03 07:42:34 +0200

    document readers with json and Java

---
+++ 05-etl-pipeline/document-readers-json-ollama/src/main/java/com/rca/jsonreaderollama/DocumentReadersJsonOllamaApplication.java
@@ -0,0 +1,13 @@
+package com.rca.jsonreaderollama;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentReadersJsonOllamaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DocumentReadersJsonOllamaApplication.class, args);
+    }
+
+}
---
+++ 05-etl-pipeline/document-readers-json-ollama/src/test/java/com/rca/jsonreaderollama/DocumentReadersJsonOllamaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rca.jsonreaderollama;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentReadersJsonOllamaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
